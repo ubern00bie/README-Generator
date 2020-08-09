13e9b983950b0411e0ab2139629b64556eec5973
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-08-09 18:32:41 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# README-Generator
-README-Generator
+
+  # Title: README Generator
+>[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  This project generates a polished README document using user inputs
+
+  ## Table of Contents
+  - [Heading](## Description)
+  - [Heading](## Installation-Instructions)
+  - [Heading](## Contribution-Guidelines)
+  - [Heading](## Usage-Information)
+  - [Heading](## Testing-Instructions)
+  - [Heading](## Feedback)
+  - [Heading](## License)
+
+
+  ## Installation-Instructions
+  To install, download the folder, open the terminal within the folder's directory, install NPM ('npm install') then install the required moduls (Inquirer, FS, and Util). Then run the README generator from the terminal by using the 'node index' command.
+
+  ## Usage-Information
+  This is used to generate a README file based on the user's inputs
+
+  ## Contribution-Guidelines
+  If you would like to contribute to this project feel free to reach out to me at the contact information listed below.
+
+  ## Testing-Instructions
+  
+  >Test 1
+  >Test 2
+  >Test 3
+
+  ## Feedback 
+  I can be reached at I can be reached at jbenningfield.dev@gmail.com for any additional questions or information about this README generator. for any additional questions or information about this README generator.
+  Find me on GitHub here: https://www.github.com/ubern00bie
+  
+  ## License
+  Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,135 @@
+// modules required to run
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+
+//promisify from utility applied to writeFile from 'fs'
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// run prompts with inquirer to get generate user data
+function getUserData() {
+  return inquirer.prompt([
+  {
+    type:"input",
+    message:"Project Title: ",
+    name:"title"
+  },
+  {
+    type:"input",
+    message:"Project Description: ",
+    name:"description"
+  },
+  {
+    type:"input",
+    message:"Installation Information: ",
+    name:"installation"
+  },
+  {
+    type:"input",
+    message:"Usage: ",
+    name:"usage"
+  },
+  { 
+    type:"list",
+    message:"License: ",
+    name:"license",
+    choices: ['MIT','GNU','BSD']
+  },
+  {
+    type:"input",
+    message:"Contribution Information: ",
+    name:"contribution"
+  },
+  {
+    type:"input",
+    message:"Tests: ",
+    name:"tests"
+  },
+  {
+    type:"input",
+    message:"Contact info - E-mail Address: ",
+    name:"contactinfo"
+  },
+  {
+    type:"input",
+    message:"GitHub username: ",
+    name:"github"
+  }
+])
+}
+
+//generate README text using 'template literals'
+function generateText(answers,badge) {
+  console.log("this one" + licenseTxt);
+  return `
+  # Title: ${answers.title}
+>${badge}
+
+  ## Description
+  ${answers.description}
+
+  ## Table of Contents
+  - [Heading](## Description)
+  - [Heading](## Installation-Instructions)
+  - [Heading](## Contribution-Guidelines)
+  - [Heading](## Usage-Information)
+  - [Heading](## Testing-Instructions)
+  - [Heading](## Feedback)
+  - [Heading](## License)
+
+
+  ## Installation-Instructions
+  ${answers.installation}
+
+  ## Usage-Information
+  ${answers.usage}
+
+  ## Contribution-Guidelines
+  ${answers.contribution}
+
+  ## Testing-Instructions
+  ${answers.tests}
+  >Test 1
+  >Test 2
+  >Test 3
+
+  ## Feedback 
+  I can be reached at ${answers.contactinfo} for any additional questions or information about this README generator.
+  Find me on GitHub here: https://www.github.com/${answers.github}
+  
+  ## License
+  ${licenseTxt}
+`
+}
+
+var licenseTxt;
+var badge;
+async function init() {
+  try {
+    const answers = await getUserData();
+    
+    if (answers.license === "MIT"){
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)'
+    }
+    else if(answers.license === "GNU"){
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    }
+    else if(answers.license === "BSD"){
+      badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    }
+    else { 
+      badge = 'License choice invalid'
+    }
+    licenseTxt = await readFileAsync('license' + answers.license + '.txt', 'utf8');
+    const README = generateText(answers,badge);
+    await writeFileAsync("README.md", README);
+    console.log("Successfully wrote README file");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+init();
+
+
